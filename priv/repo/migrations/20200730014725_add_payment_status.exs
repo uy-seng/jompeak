cd29f82408d4195c1fdf05d966bc745eb2205dc1
ashura_44056 <62083650+nova44056@users.noreply.github.com>
2020-08-03 16:51:10 +0700

    Delete 20200730014725_add_payment_status.exs

--- priv/repo/migrations/20200730014725_add_payment_status.exs
+++
@@ -1,9 +0,0 @@
-defmodule Jompeak.Repo.Migrations.CreateRecords do
-  use Ecto.Migration
-
-  def change do
-    alter table(:records) do
-      add :payment_status, :boolean, default: false
-    end
-  end
-end
